247b312cb52774b9dfbca778e4e3729d8579e4f3
andrei-cojocariu <cojocariu.andrei@gmail.com>
2023-03-07 12:24:45 +0200

    add view

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -24,7 +24,7 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToDashboard('View Fruits', 'fa fa-home');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
